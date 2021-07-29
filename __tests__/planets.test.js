2b0e359339289a28affcff722d00858508f2a477
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-29 15:19:42 -0700

    add mercury test

---
+++ __tests__/planets.test.js
@@ -0,0 +1,16 @@
+const { isPlainObject } = require("jquery");
+
+
+
+describe('Mercury', () => {
+
+  let mercury;
+
+  beforeEach(() => {
+    mercury = new Mercury();
+  });
+
+  test('should correctly calculate users earth age to their mercury age', () => {
+    
+  })
+})

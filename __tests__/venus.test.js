b38ddb83c140dc5c8d87d0338cba3d1ac308e84e
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-29 15:52:38 -0700

    add venus test

---
+++ __tests__/venus.test.js
@@ -0,0 +1,14 @@
+import Venus from "../src/venus"
+
+describe('Venus', () => {
+
+  let venus;
+
+  beforeEach(() => {
+    venus = new Venus();
+  });
+
+  Test('should correctly calculate users age to their venus age', () => {
+    
+  })
+})

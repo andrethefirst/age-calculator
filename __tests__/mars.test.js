14282ae2406533ebe223a32d5d3c202661e8aaeb
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-29 16:11:57 -0700

    add mars tests

---
+++ __tests__/mars.test.js
@@ -0,0 +1,14 @@
+import Mars from '../src/mars'
+
+describe('Mars', () => {
+
+  let mars;
+
+  beforeEach(() => {
+    mars = new Mars();
+  });
+
+  test('should correctly calculate users age to their mars age', => {
+    
+  })
+})

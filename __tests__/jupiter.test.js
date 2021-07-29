4e645d07a84e3f0070bb8bd6c502bc7f5d4f8d0c
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-29 16:24:25 -0700

    add jupiter test

---
+++ __tests__/jupiter.test.js
@@ -0,0 +1,14 @@
+import Jupiter from '../src/jupiter'
+
+describe('Jupiter', () => {
+
+  let jupiter;
+
+  beforeEach(() => {
+    jupiter = new Jupiter();
+  });
+
+  test('should correctly calculate users age to their jupiter age', => {
+
+  });
+})

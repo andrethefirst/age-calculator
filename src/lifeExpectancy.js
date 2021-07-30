aa8b4b4da7054ebcaa422ee4d0bf2dfce3194c31
Andre Velasquez <andrethefirst21@gmail.com>
2021-07-30 16:08:46 -0700

    good fail life expectancy test

---
+++ src/lifeExpectancy.js
@@ -0,0 +1,14 @@
+export default class LifeExpectancy {
+  constructor(gender, alcConsum, smoker) {
+    let avgLifeSpan = ;
+    if(gender === 'male') {
+      avgLifeSpan -= 
+    } else if (gender === 'female') {
+      avgLifeSpan += ;
+    } else if (alcConsum === 'yes') {
+      avgLifeSpan -= ;
+    } else if (smoker === 'yes') {
+      avgLifeSpan -= ;
+    }
+  }
+}
